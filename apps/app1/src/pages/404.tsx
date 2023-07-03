9ff647f463e96bae2806dbecbc4a437fddb7b948
Ravi <toritroniks@users.noreply.github.com>
2023-07-03 12:18:57 +0900

    rewrite test

---
+++ apps/app1/src/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function Error404() {
+  return (
+    <>
+      <main>
+        <h1>App 1 404</h1>
+      </main>
+    </>
+  );
+}
---
+++ apps/app2/src/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function Error404() {
+  return (
+    <>
+      <main>
+        <h1>App 2 404</h1>
+      </main>
+    </>
+  );
+}
